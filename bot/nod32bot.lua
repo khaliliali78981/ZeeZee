07a2779a4c89b73657fadebb2de649cdf8ff5c78
amirkingred <amir.a.m.h48@gmail.com>
2016-03-03 17:34:30 +0330

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -244,14 +244,14 @@ function create_config( )
     "time",
     "version"
     },
-	    sudo_users = {158990680},--Sudo users
+	    sudo_users = {123755887},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
 https://github.com/BH-YAGHI/NOD32-BOT.git
 
-channel : @Nod32team
-sodu : @behrooZyaghi
+channel : @telejian100
+sodu : @amir13137878
 ]],
     help_text_realm = [[
 Realm Commands:
@@ -316,11 +316,11 @@ Only sudo users can run this command
 !bc 123456789 Hello !
 This command will send text to [group_id]
 
-ch: @Nod32team
+ch: @telejian100
 
 ]],
     help_text = [[
-NOD32 Commands list :
+TelejianCommands list :
  
 1-banhammer list ^
  
@@ -517,9 +517,10 @@ NOD32 Commands list :
 you can use both "/" and "!" 
 .شما میتوانید از ! و / استفاده کنید
 
-channel: @Nod32Team
+brodcast for telejianbot🔥
+اطلاع رسانی ربات تله جیان🔥
+@telejian100
 
-G00D LUCK ^_^
 ]]
    }
   serialize_to_file(config, './data/config.lua')
